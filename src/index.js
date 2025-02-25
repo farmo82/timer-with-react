c60947deebcf75c7ace21b68e08d1274c7ada56e
FM <fmotaharinejadf@gmail.com>
2025-02-25 09:11:42 +0330

    timer

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom/client';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <h1>hello timer</h1>
-);
-// در نسخه قدیمی
-// import ReactDOM from 'react-dom';
-// ReactDOM.render(<h1>hello timer</h1>, document.getElementById('root'));
+
+const tick = ()=>{
+  const elem = (
+    <div>
+      <h1>hello timer</h1>
+      <h2>
+        It is {new Date().toLocaleTimeString()}
+      </h2>
+    </div>
+  )
+
+  root.render(
+    elem
+  );
+}
+
+setInterval(()=>{  // برای اینکه بدون زدن f5 زمان رو نشون بده
+    tick();
+  }, 1000)
