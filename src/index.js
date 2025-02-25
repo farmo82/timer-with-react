fe38d93cc539e29ea6a151eba3fd4185f83bee6a
FM <fmotaharinejadf@gmail.com>
2025-02-25 09:59:58 +0330

    component with -new- class

--- src/index.js
+++ src/index.js
@@ -4,18 +4,25 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+class App extends React.Component{  //this is component
+  render(){
+    return(
+      <div>
+        <h1>hello timer</h1>
+        <h2>
+          It is {new Date().toLocaleTimeString()}
+        </h2>
+      </div>
+    )
+  }
+}
+
+let elem = new App()
+
 const tick = ()=>{
-  const elem = (
-    <div>
-      <h1>hello timer</h1>
-      <h2>
-        It is {new Date().toLocaleTimeString()}
-      </h2>
-    </div>
-  )
 
   root.render(
-    elem
+    elem.render()
   );
 }
 
