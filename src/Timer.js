34f2f2e6838011c86f838d5a3c225439f8aeb437
FM <fmotaharinejadf@gmail.com>
2025-02-25 21:03:26 +0330

    #11_Yard cycle react - componentDidMount function

--- src/Timer.js
+++ src/Timer.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom/client';
 import './style.css'
 
 
+var interval;
+
+
 class Timer extends React.Component{  //component
     constructor(){
       super();
@@ -11,14 +14,15 @@ class Timer extends React.Component{  //component
         time: new Date().toLocaleTimeString()
       }
     }
+    componentDidMount(){  //یک بار اول کار میسازه
+        interval = setInterval(()=>{ //interval برای تغییر state
+            this.setState({
+              time: new Date().toLocaleTimeString()
+            })
+          }, 1000)
+    }
   
     render(){
-      setInterval(()=>{ //interval برای تغییر state
-        this.setState({
-          time: new Date().toLocaleTimeString()
-        })
-      }, 1000)
-  
       return(
         <h2 className='timer'>
           It is {this.state.time}
