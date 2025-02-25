c1db0e81efcd1094d1c0a7d7955c05cfcc9b1276
FM <fmotaharinejadf@gmail.com>
2025-02-25 10:17:40 +0330

    #8_more component

--- src/index.js
+++ src/index.js
@@ -4,15 +4,37 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+class Timer extends React.Component{
+  render(){
+    return(
+      <h2>
+        It is {new Date().toLocaleTimeString()}
+      </h2>
+    )
+  }
+}
+
+class Text extends React.Component{
+  render(){
+    return(
+      <h1>hello timer</h1>
+    )
+  }
+}
+
 class App extends React.Component{  //this is component
   render(){
     return(
-      <div>
-        <h1>hello timer</h1>
-        <h2>
-          It is {new Date().toLocaleTimeString()}
-        </h2>
+      // حتما باید یک تگ باز و بسته باشد. میتواند تگ بی نام باشد
+      <div>  
+        <Text/>
+        <Timer/>
       </div>
+
+      // <>    با بالا برابر است
+      //   <Text/>
+      //   <Timer/>
+      // </>
     )
   }
 }
